c8c4d2aa37bba0fb3ce2969e551fa26af1aca20f
Roberto Mafra <mximoroberto@yahoo.com.br>
2019-07-23 13:33:16 -0300

    Aprendendo a usar Enum e separando em pastas as classes para deixar mais organizado

---
+++ IntermediateCorse/Program.cs
@@ -0,0 +1,28 @@
+﻿using IntermediateCorse.Entities;
+using IntermediateCorse.Entities.Enums;
+using System;
+
+namespace IntermediateCorse
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Order order = new Order
+            {
+                Id = 1080,
+                Moment = DateTime.Now,
+                Status = OrderStatus.PendingPayment
+            };
+
+            Console.WriteLine(order);
+
+            string txt = OrderStatus.PendingPayment.ToString();
+
+            OrderStatus os = Enum.Parse<OrderStatus>("Delivered");
+
+            Console.WriteLine(txt);
+            Console.WriteLine(os);
+        }
+    }
+}
